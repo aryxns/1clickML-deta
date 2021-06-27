9896fc862d4efb539d80ac714d00fcc08daad587
Aryan Sharma <52833999+aryxns@users.noreply.github.com>
2021-06-28 01:39:38 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
 import {BrowserRouter,Switch,Route} from "react-router-dom";
-import Home from "./components/HomePage";
 import Experiment from "./components/experiment";
 import * as dfd from "danfojs/src/index";
 
@@ -11,8 +10,7 @@ function App() {
     <div className="App w-4/5">
       <BrowserRouter>
       <Switch>
-        <Route path="/" component={Home} exact/>
-        <Route path="/ex" component={Experiment}/>
+        <Route path="/" component={Experiment}/>
       </Switch>
       </BrowserRouter>
     </div>
